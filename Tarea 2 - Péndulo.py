250f1a4f95ef1748fd10d52b1fb471e44afc9497
BrayanAmor8 <89361938+BrayanAmor8@users.noreply.github.com>
2021-09-10 17:07:12 -0500

    Update Tarea 2 - Péndulo.py

--- Tarea 2 - Péndulo.py
+++ Tarea 2 - Péndulo.py
@@ -5,7 +5,6 @@
 # Brayan Amorocho - Santiago Montes - Juliana Andrade
 # 
 
-# In[106]:
 
 
 #Importando bibliotecas
@@ -15,13 +14,10 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 
-# In[90]:
 
 
 #Para importar datos de tracker
 
-
-# In[124]:
 
 
 #Definición de variables:
@@ -32,7 +28,6 @@ L = [ 1, 2, 1, 2]
 #Hay 4 posibles casos. Inicialmente se utilizará un ángulo pequeño
 
 
-# In[128]:
 
 
 #Fuentes bonitas para las gráficas futuras
@@ -49,9 +44,6 @@ font2 = {"family" : "serif",
          }
 
 
-# In[92]:
-
-
 #Encontrando los ángulos donde sen(x) = x con un margen de error del 3%:
 x = np.linspace(0,1,100)
 error = []
@@ -64,8 +56,6 @@ for i in range(0, len(x)):
         print("Es posible aproximar hasta que el ángulo valga", x[i-1])
         break
 
-
-# In[163]:
 
 
 time = np.linspace(0, 10, 100000)
@@ -92,12 +82,9 @@ for j in range(0,4): #Para hacer de una vez los 4 casos
     
 
 
-# In[166]:
-
-
 def conjunto_graficas(n):
     """
-    Conjunto de gráficas para el caso 1, en un mundo sin fricción
+    Conjunto de gráficas, en un mundo sin fricción
     """
     #Angulo en grados
     plt.figure()
@@ -136,104 +123,13 @@ def conjunto_graficas(n):
     
 
 
-# In[167]:
-
-
 conjunto_graficas(0)
-
-
-# In[168]:
-
 
 conjunto_graficas(1)
 
-
-# In[ ]:
-
-
 conjunto_graficas(2)
 
-
-# In[165]:
-
-
 conjunto_graficas(3)
-
-
-# In[116]:
-
-
-#Caso 2 con m2 y L2:
-time = np.linspace(0, 10, 100000)
-a = np.zeros(len(time))  #aceleración angular
-w = np.zeros(len(time))  #velocidad angular
-T = np.zeros(len(time))  #Tensión
-angulo = np.zeros(len(time))
-angulo[0] = 0.42
-g = 9.78
-for i in range(0,len(time)-1):
-    cambio = time[i+1]-time[i]
-    a[i]   = g*angulo[i] / L2
-    T[i]   = -m2*w[i]*w[i]*L2 + m2*g*math.cos(angulo[i])
-    w[i+1] = w[i] + a[i] * (cambio)
-    angulo[i+1] = angulo[i] - w[i]*(cambio) + a[i]* ((cambio**2) / 2)
-angulo_grad = angulo * 180 / math.pi #Pasar a grados para hacer más intuitiva la gráfica
-
-
-# In[117]:
-
-
-"""
-Conjunto de gráficas para el caso 2, en un mundo sin fricción
-"""
-#Angulo en grados
-plt.figure()
-plt.plot(time,angulo_grad, "k")
-plt.title("Movimiento gradual", fontdict = font)
-plt.ylabel(r"Ángulo $\theta°$", fontdict = font2)
-plt.xlabel("Tiempo ($s$)", fontdict=font2)
-plt.grid(linestyle="--")
-
-#Angulo en radianes
-plt.figure()
-plt.plot(time,angulo, "k")
-plt.title("Movimiento gradual", fontdict = font)
-plt.ylabel(r"Ángulo $\theta~(Rad)$", fontdict = font2)
-plt.xlabel("Tiempo ($s$)", fontdict=font2)
-plt.grid(linestyle="--")
-
-#Movimiento Angular
-plt.figure()
-plt.plot(time,w, "g")
-plt.title("Velocidad Angular en función del tiempo", fontdict = font)
-plt.ylabel(r"Velocidad angular $\theta~(Rad)$", fontdict = font2)
-plt.xlabel("Tiempo ($s$)", fontdict=font2)
-plt.grid(linestyle="--")
-
-#Comparación
-plt.figure()
-plt.plot(time,angulo, "k", label="Ángulo")
-plt.plot(time,w, "g", label = "Velocidad Angular")
-plt.legend(loc = "best")
-plt.title("Movimiento gradual", fontdict = font)
-plt.xlabel("Tiempo ($s$)", fontdict=font2)
-plt.grid(linestyle="--")
-
-
-# In[99]:
-
-
-
-
-
-# In[104]:
-
-
-
-
-
-# In[103]:
-
 
 
 
