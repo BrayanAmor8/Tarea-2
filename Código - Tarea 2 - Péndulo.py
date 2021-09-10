194d3729b6110c1bf25d6f323df50a0c1550a481
BrayanAmor8 <89361938+BrayanAmor8@users.noreply.github.com>
2021-09-10 17:05:26 -0500

    Update Código - Tarea 2 - Péndulo.py

--- Código - Tarea 2 - Péndulo.py
+++ Código - Tarea 2 - Péndulo.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-
-
+# # Código - Estimación de la gravedad a partir del movimiento de un péndulo
+# Brayan Amorocho - Santiago Montes - Juliana Andrade
+# 
 
 #Importando bibliotecas
 import numpy as np
@@ -11,18 +12,30 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 
-
-
-
 #Para importar datos de tracker
-
-
 
 
 #Definición de variables:
 m1, m2, m3, m4 = 0.6, 0.6, 0.3, 0.3
 L1, L2, L3, L4 = 1  , 2,  1,  2
+#m = [0.6, 0.6, 0.3, 0.3]
 #Hay 4 posibles casos. Inicialmente se utilizará un ángulo pequeño
+
+
+
+#Fuentes bonitas para las gráficas futuras
+font = {"family" : "serif",
+        "color"  : "darkred",
+        "weight" : "bold",
+        "size"   : 16,
+        }
+
+font2 = {"family" : "serif",
+         "color"  : "black",
+         "weight" : "normal",
+         "size"   : 12,
+         }
+
 
 
 
@@ -39,30 +52,115 @@ for i in range(0, len(x)):
         break
 
 
-
 #Caso 1, con m1 y L1:
-time = np.linspace(0, 1, 1000)
+time = np.linspace(0, 10, 100000)
 a = np.zeros(len(time))  #aceleración angular
 w = np.zeros(len(time))  #velocidad angular
 T = np.zeros(len(time))  #Tensión
 angulo = np.zeros(len(time))
-angulo[0] = 0.1
+angulo[0] = 0.42
 g = 9.78
 for i in range(0,len(time)-1):
     cambio = time[i+1]-time[i]
     a[i]   = g*angulo[i] / L1
     T[i]   = -m1*w[i]*w[i]*L1 + m1*g*math.cos(angulo[i])
     w[i+1] = w[i] + a[i] * (cambio)
-    angulo[i+1] = angulo[i] + w[i]*(cambio) + a[i]* ((cambio**2) / 2)
-    if T[i]<0:
-        print("El tiempo es de", time[i])
-        print("La aceleración angular máxima es de", a[i])
-        print("La velocidad angular maxima es de", w[i])
-        print("El ángulo máximo es de", angulo[i])
-        break
+    angulo[i+1] = angulo[i] - w[i]*(cambio) + a[i]* ((cambio**2) / 2)
+angulo_grad = angulo * 180 / math.pi #Pasar a grados para hacer más intuitiva la gráfica
     
     
+   
+"""
+Conjunto de gráficas para el caso 1, en un mundo sin fricción
+"""
+#Angulo en grados
+plt.figure()
+plt.plot(time,angulo_grad, "k")
+plt.title("Movimiento gradual", fontdict = font)
+plt.ylabel(r"Ángulo $\theta°$", fontdict = font2)
+plt.xlabel("Tiempo ($s$)", fontdict=font2)
+plt.grid(linestyle="--")
 
+#Angulo en radianes
+plt.figure()
+plt.plot(time,angulo, "k")
+plt.title("Movimiento gradual", fontdict = font)
+plt.ylabel(r"Ángulo $\theta~(Rad)$", fontdict = font2)
+plt.xlabel("Tiempo ($s$)", fontdict=font2)
+plt.grid(linestyle="--")
+
+#Movimiento Angular
+plt.figure()
+plt.plot(time,w, "g")
+plt.title("Velocidad Angular en función del tiempo", fontdict = font)
+plt.ylabel(r"Velocidad angular $\theta~(Rad)$", fontdict = font2)
+plt.xlabel("Tiempo ($s$)", fontdict=font2)
+plt.grid(linestyle="--")
+
+#Comparación
+plt.figure()
+plt.plot(time,angulo, "k", label="Ángulo")
+plt.plot(time,w, "g", label = "Velocidad Angular")
+plt.legend(loc = "best")
+plt.title("Movimiento gradual", fontdict = font)
+plt.xlabel("Tiempo ($s$)", fontdict=font2)
+plt.grid(linestyle="--")
+
+
+#Caso 2 con m2 y L2:
+time = np.linspace(0, 10, 100000)
+a = np.zeros(len(time))  #aceleración angular
+w = np.zeros(len(time))  #velocidad angular
+T = np.zeros(len(time))  #Tensión
+angulo = np.zeros(len(time))
+angulo[0] = 0.42
+g = 9.78
+for i in range(0,len(time)-1):
+    cambio = time[i+1]-time[i]
+    a[i]   = g*angulo[i] / L2
+    T[i]   = -m2*w[i]*w[i]*L2 + m2*g*math.cos(angulo[i])
+    w[i+1] = w[i] + a[i] * (cambio)
+    angulo[i+1] = angulo[i] - w[i]*(cambio) + a[i]* ((cambio**2) / 2)
+angulo_grad = angulo * 180 / math.pi #Pasar a grados para hacer más intuitiva la gráfica
+
+
+
+
+"""
+Conjunto de gráficas para el caso 2, en un mundo sin fricción
+"""
+#Angulo en grados
+plt.figure()
+plt.plot(time,angulo_grad, "k")
+plt.title("Movimiento gradual", fontdict = font)
+plt.ylabel(r"Ángulo $\theta°$", fontdict = font2)
+plt.xlabel("Tiempo ($s$)", fontdict=font2)
+plt.grid(linestyle="--")
+
+#Angulo en radianes
+plt.figure()
+plt.plot(time,angulo, "k")
+plt.title("Movimiento gradual", fontdict = font)
+plt.ylabel(r"Ángulo $\theta~(Rad)$", fontdict = font2)
+plt.xlabel("Tiempo ($s$)", fontdict=font2)
+plt.grid(linestyle="--")
+
+#Movimiento Angular
+plt.figure()
+plt.plot(time,w, "g")
+plt.title("Velocidad Angular en función del tiempo", fontdict = font)
+plt.ylabel(r"Velocidad angular $\theta~(Rad)$", fontdict = font2)
+plt.xlabel("Tiempo ($s$)", fontdict=font2)
+plt.grid(linestyle="--")
+
+#Comparación
+plt.figure()
+plt.plot(time,angulo, "k", label="Ángulo")
+plt.plot(time,w, "g", label = "Velocidad Angular")
+plt.legend(loc = "best")
+plt.title("Movimiento gradual", fontdict = font)
+plt.xlabel("Tiempo ($s$)", fontdict=font2)
+plt.grid(linestyle="--")
 
 
 
